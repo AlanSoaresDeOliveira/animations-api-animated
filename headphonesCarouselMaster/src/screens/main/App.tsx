c56b3940be184b5978a77453e45d9260c81b3d30
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-07-30 16:05:40 -0300

    Create the Circle component

--- headphonesCarouselMaster/src/screens/main/App.tsx
+++ headphonesCarouselMaster/src/screens/main/App.tsx
@@ -17,6 +17,40 @@ const LOGO_WIDTH = 220;
 const LOGO_HEIGHT = 40;
 const DOT_SIZE = 40;
 const TICKER_HEIGHT = 40;
+const CIRCLE_SIZE = width * 0.6;
+
+const Circle = ({scrollX}) => {
+  return (
+    <View style={[StyleSheet.absoluteFillObject, styles.circleContainer]}>
+      {data.map(({color}, index) => {
+        const inputRange = [
+          (index - 0.55) * width,
+          index * width,
+          (index + 0.55) * width,
+        ];
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [0, 1, 0],
+          extrapolate: 'clamp',
+        });
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0, 0.2, 0],
+        });
+        return (
+          <Animated.View
+            key={index}
+            style={[
+              styles.circle,
+
+              {backgroundColor: color, opacity, transform: [{scale}]},
+            ]}
+          />
+        );
+      })}
+    </View>
+  );
+};
 
 const Ticker = ({scrollX}) => {
   const inputRange = [-width, 0, width];
@@ -109,6 +143,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" hidden />
+      <Circle scrollX={scrollX} />
       <Animated.FlatList
         keyExtractor={(item) => item.key}
         data={data}
@@ -225,5 +260,16 @@ const styles = StyleSheet.create({
     lineHeight: TICKER_HEIGHT,
     textTransform: 'uppercase',
     fontWeight: '800',
+  },
+  circleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  circle: {
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+    position: 'absolute',
+    top: '15%',
   },
 });
