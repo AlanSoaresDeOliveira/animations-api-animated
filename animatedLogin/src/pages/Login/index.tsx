90d150095dbb6224757b4d18eaadac99da362c75
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-08-02 12:19:57 -0300

    Add src file and pages inside

---
+++ animatedLogin/src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import {Animated} from 'react-native';
+
+import {Container} from './styles';
+
+const Login: React.FC = () => {
+  return <Container />;
+};
+
+export default Login;
